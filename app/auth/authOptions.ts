5fef1b6aa824318569f0b1b9ae34dddd8fa0bcc4
Mehdi Vaez <mahdivaeztehrani81@gmail.com>
2024-10-05 07:38:00 -0700

    setup the prisma and next-auth

---
+++ app/auth/authOptions.ts
@@ -0,0 +1,31 @@
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from '@/prisma/client';
+import { NextAuthOptions } from 'next-auth';
+
+const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    // Customize JWT callback to include additional data
+    async jwt({ token, account, user }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    }}}
+
+
+
+export default authOptions;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,16 @@
+"use client"
 import Link from "next/link"
 import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import logo from "@/public/logo.png"  // Assuming this is the correct path to your logo image
+import { signIn } from "next-auth/react"
 
 export function Navbar() {
   return (
     <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-gray-950/90">
       <div className="w-full max-w-7xl mx-auto px-4">
         <div className="flex justify-between h-14 items-center">
-        <Link href="/" className="text-2xl font-bold text-primary">
+          <Link href="/" className="text-2xl font-bold text-primary">
             RecipeShare
           </Link>
           <nav className="hidden md:flex gap-4">
@@ -39,13 +41,13 @@ export function Navbar() {
               prefetch={false}
             >
               Profile
-              </Link>
+            </Link>
           </nav>
           <div className="flex items-center gap-4">
-            <Button variant="outline" size="sm">
+            {/* Use onClick to trigger the signIn action */}
+            <Button variant="outline" size="sm" onClick={() => signIn()}>
               Sign in
             </Button>
-            
           </div>
         </div>
       </div>
